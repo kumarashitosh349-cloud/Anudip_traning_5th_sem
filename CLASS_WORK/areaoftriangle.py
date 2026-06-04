174fd39cb1403bb93cc228f810e3a93ef47e72d1
Ashitosh Kumar <kumarashitosh349@gmail.com>
2026-06-04 11:43:26 +0530

    first commit

---
+++ CLASS_WORK/areaoftriangle.py
@@ -0,0 +1,12 @@
+present=0
+absent=0
+n=int(input("enter total number of students"))
+for i in range(n):
+    s=input("p for present or a for absent")
+    if s=="p":
+        present+=1
+    elif s=="a":
+        absent+=1
+
+print("total present:", present)
+print("total absent:", absent)
