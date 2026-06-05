495d5b51a91973b52d0602df41597de9427c92dd
Ashitosh Kumar <kumarashitosh349@gmail.com>
2026-06-05 10:16:40 +0530

    first commit

--- LAB_WORK/4_june/primeanalyser.py
+++ LAB_WORK/4_june/primeanalyser.py
@@ -1,12 +1,12 @@
 n = int(input("enter a number: "))
 count = 0
 
-# Count divisors
+
 for i in range(1, n + 1):
     if n % i == 0:
         count += 1
 
-# Check if prime and print factors if not
+
 if count == 2:
     print("Prime")
 else:
